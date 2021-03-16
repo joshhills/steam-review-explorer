1ad3a4fc09fcf8a9a45f4b130302791940fc9ee1
joshhills <joshhills@users.noreply.github.com>
2021-03-16 01:44:08 +0000

    deploy: 23e7e21405ead2fc7ef832311cd66f814b046d58

---
+++ _next/static/OHfdp6wGQ5jPQigKJyk4R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F29107295.284f67362ddaed3cc7d2.js","static\u002Fchunks\u002Fpages\u002Findex-dd0c251039b73b25d821.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ce418687d3fddc5638e0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

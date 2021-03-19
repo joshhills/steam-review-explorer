41e88615183406f7f73a0a59e59de6669fe1c5de
Josh Hills <joshuamarcushills@gmail.com>
2021-03-19 12:16:10 +0100

    Fixed donate component

--- components/Donate.tsx
+++ components/Donate.tsx
@@ -1,3 +1,3 @@
 export default function Donate() {
-    return (<a href='https://ko-fi.com/I2I51WSPZ' target='_blank'><img height='36' style={{border:'0px',height:'36px'}} src='https://cdn.ko-fi.com/cdn/kofi1.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>)
+    return (<a href='https://ko-fi.com/I2I51WSPZ' target='_blank'><img height='36' style={{border:'0px',height:'36px'}} src='https://cdn.ko-fi.com/cdn/kofi1.png?v=2' alt='Buy Me a Coffee at ko-fi.com' /></a>)
 }

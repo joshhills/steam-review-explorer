67dd1bd225b952d9ebf54034d2c9597f12dc293d
Josh Hills <joshuamarcushills@gmail.com>
2021-04-02 02:18:01 +0200

    404 shenanigans

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,22 @@
+import Head from "next/head";
+import React from "react";
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (<>
+        <Head>
+            <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+                var pathSegmentsToKeep = 1;
+
+                var l = window.location;
+                l.replace(
+                  l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+                  l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+                  l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+                  (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+                  l.hash
+                );
+            `}}></script>
+        </Head>
+        <h1>404 - Page Not Found</h1>
+    </>)
 }
